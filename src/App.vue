290a51d2b3a67cf8bba5a98c49be811fd331235f
dlrlsud@naver.com <dlrlsud@naver.com>
2021-11-25 14:02:56 +0900

    Footer Complete

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,29 @@
 <template>
-  <div>
+  <div class="app-wrapper">
     <Header />
-    <router-view />
+    <router-view class="router-view"/>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from './components/inc/Header.vue'
+import Footer from './components/inc/Footer.vue'
 
 export default {
   name: 'App',
-  components: { Header }
+  components: { Header, Footer }
 }
 </script>
 
 <style lang="scss">
-ul {
-  line-height : 0;
+.app-wrapper {
+  height: 100vh;
+  @include flex;
+  @include flexCol;
+  .router-view {
+    border: 2px solid red;
+    flex-grow: 1;
+  }
 }
 </style>
